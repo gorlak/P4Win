dc5655823242877307252c2deaad79775d1e7721
Geoff Evans <geoff@flummoxed.org>
2019-05-26 15:23:13 -0700

    Update premake.bat to build with msbuild

--- premake.bat
+++ premake.bat
@@ -1,7 +1,9 @@
 @echo off
 
+for /f %%f in ( '"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -property catalog_productLineVersion' ) do set VSVERSION=%%f
+
 pushd %~dp0Dependencies\premake
-if not exist bin\release\premake5.exe nmake -f Bootstrap.mak windows
+if not exist bin\release\premake5.exe nmake -f Bootstrap.mak MSDEV=vs%VSVERSION% windows-msbuild
 popd
 
 "%~dp0Dependencies\premake\bin\release\premake5.exe" %*
