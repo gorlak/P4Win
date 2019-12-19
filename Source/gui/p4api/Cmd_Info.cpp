a0ecd62b28d8005f04bc9e19e035af4de666f672
Daniel Jennings <danielj@valvesoftware.com>
2019-12-19 10:30:54 -0800

    We know, but don't need, the "Broker address" from p4 info.

--- Source/gui/p4api/Cmd_Info.cpp
+++ Source/gui/p4api/Cmd_Info.cpp
@@ -96,6 +96,8 @@ void CCmd_Info::OnOutputInfo(char level, LPCTSTR data, LPCTSTR msg)
         ;   // we know about it, but don't care about it
     else if(tag == _T("Changelist server"))
         ;   // we know about it, but don't care about it
+    else if(tag == _T("Broker address"))
+        ;   // we know about it, but don't care about it
     else
     {
         ASSERT(0);   // info not recognized
